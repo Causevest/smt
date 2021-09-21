c06b06ae13dad7f8737e813a59ef3f6ee512d44f
Ben Mahala <BenMahalaD@gmail.com>
2021-09-20 23:33:20 -0400

    should allow full integration with node

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/causevest/smt
 
 go 1.14
+
+require golang.org/x/crypto v0.0.0-20210920023735-84f357641f63
--- mapstore.go
+++ mapstore.go
@@ -1,7 +1,11 @@
 package smt
 
 import (
+	"bytes"
+	"encoding/gob"
 	"fmt"
+
+	"golang.org/x/crypto/sha3"
 )
 
 // MapStore is a key-value store.
@@ -9,6 +13,7 @@ type MapStore interface {
 	Get(key []byte) ([]byte, error)     // Get gets the value for a key.
 	Set(key []byte, value []byte) error // Set updates the value for a key.
 	Delete(key []byte) error            // Delete deletes a key.
+	Export() ([]byte, error)            // exports the map into a byte array
 }
 
 // InvalidKeyError is thrown when a key that does not exist is being accessed.
@@ -46,6 +51,30 @@ func (sm *SimpleMap) Set(key []byte, value []byte) error {
 	return nil
 }
 
+// Export dumps the map into a gob serial
+func (sm *SimpleMap) Export() ([]byte, error) {
+	serial, err := GobEncode(sm.m)
+	return serial, err
+}
+
+// Gob is used for encoding internal state
+// and data. Json.Marshal is used for
+// p2p communciation and RPC
+// Works just like json.Marashal
+func GobEncode(v interface{}) ([]byte, error) {
+	b := new(bytes.Buffer)
+	err := gob.NewEncoder(b).Encode(v)
+	return b.Bytes(), err
+}
+
+// Works just like json.Unmarashal
+func GobDecode(b []byte, v interface{}) error {
+	buf := new(bytes.Buffer)
+	buf.Write(b)
+	err := gob.NewDecoder(buf).Decode(v)
+	return err
+}
+
 // Delete deletes a key.
 func (sm *SimpleMap) Delete(key []byte) error {
 	_, ok := sm.m[string(key)]
@@ -54,4 +83,68 @@ func (sm *SimpleMap) Delete(key []byte) error {
 		return nil
 	}
 	return &InvalidKeyError{Key: key}
+}
+
+// makes a new smt using a merklemap
+// and the sha3 hash function and returns it
+func NewMerkleTrie() *SparseMerkleTree {
+
+	smn := NewSimpleMap()
+	smv := NewSimpleMap()
+
+	trie := NewSparseMerkleTree(smn, smv, sha3.New256())
+
+	return trie
+}
+
+// used to save the a Trie to statedb
+// keeps the root and map serial together
+type TrieWrap struct {
+	Root        []byte
+	NodesBytes  []byte
+	ValuesBytes []byte
+}
+
+func ImportTrie(wrap *TrieWrap) (*SparseMerkleTree, error) {
+	// takes the gob encoded map for an smt and returns the smt
+	smn, smv, err := ImportMerkleMap(wrap.NodesBytes, wrap.ValuesBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return ImportSparseMerkleTree(smn, smv, sha3.New256(), wrap.Root), nil
+}
+
+func ExportTrie(trie *SparseMerkleTree) (*TrieWrap, error) {
+	nodesBytes, err := trie.nodes.Export()
+	if err != nil {
+		return nil, err
+	}
+
+	valuesBytes, err := trie.values.Export()
+	if err != nil {
+		return nil, err
+	}
+
+	wrap := TrieWrap{
+		Root:        trie.Root(),
+		NodesBytes:  nodesBytes,
+		ValuesBytes: valuesBytes,
+	}
+	return &wrap, nil
+}
+
+func ImportMerkleMap(nodesBytes, valuesBytes []byte) (*SimpleMap, *SimpleMap, error) {
+	var smn, smv SimpleMap
+	err := GobDecode(nodesBytes, &smn.m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	err = GobDecode(valuesBytes, &smv.m)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &smn, &smv, err
 }
